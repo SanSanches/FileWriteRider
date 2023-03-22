f21e4b02120e47fd333e3dc153df721c691c5466
Александр Немков <103505723+SanSanches@users.noreply.github.com>
2023-03-22 19:40:13 +0300

    Implemented saveText in MainActivity

--- app/src/main/java/com/example/filewriterider/MainActivity.java
+++ app/src/main/java/com/example/filewriterider/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.filewriterider;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
     private final static String FILE_NAME = "content.txt";
@@ -14,4 +22,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // Сохранение файла
+    public void saveText(View view) {
+        FileOutputStream fos = null;
+        try {
+            EditText textBox = (EditText) view.findViewById(R.id.editor);
+            String text = textBox.getText().toString();
+
+
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes(StandardCharsets.UTF_8));
+            Toast.makeText(this, "Файл успешно сохранён", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if(fos != null) {
+                    fos.close();
+                }
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    // Открытие файла
+
 }
